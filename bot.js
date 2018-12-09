abd138a746c0b831c06ccf47560e4807acf9e94c
HUSSAINxB <42673535+HUSSAINxB@users.noreply.github.com>
2018-12-09 23:59:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,7 +93,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Night Server`,"http://twitch.tv/HUSSAINxB")
+client.user.setGame(`Night Server By HUSSAINxB,`,"http://twitch.tv/HUSSAINxB")
 client.user.setStatus("dnd")
 });
 
